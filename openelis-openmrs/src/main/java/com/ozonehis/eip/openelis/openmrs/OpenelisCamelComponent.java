517268657269b38cee04316f8cc747e9bac7f81d
Siddharth Vaish <sidvaish97@gmail.com>
2024-08-16 15:51:55 +0530

    Refactor openelis camel component

--- openelis-openmrs/src/main/java/com/ozonehis/eip/openelis/openmrs/OpenelisCamelComponent.java
+++ openelis-openmrs/src/main/java/com/ozonehis/eip/openelis/openmrs/OpenelisCamelComponent.java
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.ozonehis.eip.openelis.openmrs.client;
+package com.ozonehis.eip.openelis.openmrs;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
