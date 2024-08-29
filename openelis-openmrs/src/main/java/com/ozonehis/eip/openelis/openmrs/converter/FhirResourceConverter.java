f21c282b3d70d1af794858cd658345ecd3c92148
Siddharth Vaish <sidvaish97@gmail.com>
2024-08-29 15:37:39 +0530

    Clean unused converters

--- openelis-openmrs/src/main/java/com/ozonehis/eip/openelis/openmrs/converter/FhirResourceConverter.java
+++ openelis-openmrs/src/main/java/com/ozonehis/eip/openelis/openmrs/converter/FhirResourceConverter.java
@@ -8,21 +8,11 @@
 package com.ozonehis.eip.openelis.openmrs.converter;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.rest.api.MethodOutcome;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Converter;
 import org.apache.camel.Exchange;
 import org.apache.camel.converter.stream.InputStreamCache;
-import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.DiagnosticReport;
-import org.hl7.fhir.r4.model.DomainResource;
-import org.hl7.fhir.r4.model.Observation;
-import org.hl7.fhir.r4.model.Practitioner;
-import org.hl7.fhir.r4.model.ServiceRequest;
-import org.hl7.fhir.r4.model.Task;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -31,63 +21,9 @@ import org.springframework.stereotype.Component;
 public class FhirResourceConverter {
 
     @Converter
-    public static InputStream convertResourceToInputStream(DomainResource resource) {
-        FhirContext ctx = FhirContext.forR4();
-        String json = ctx.newJsonParser().encodeResourceToString(resource);
-        return new ByteArrayInputStream(json.getBytes());
-    }
-
-    @Converter
-    public static IBaseResource convertMethodOutcomeToIBaseResource(MethodOutcome outcome) {
-        if (outcome.getResource() != null) {
-            return (IBaseResource) outcome.getResource();
-        } else {
-            log.warn("The MethodOutcome does not contain a valid IBaseResource. Returning null.");
-            return null;
-        }
-    }
-
-    @Converter
-    public static Task toTask(InputStreamCache inputStreamCache, Exchange exchange) throws Exception {
-        FhirContext ctx = FhirContext.forR4();
-        String body = exchange.getContext().getTypeConverter().mandatoryConvertTo(String.class, inputStreamCache);
-        return (Task) ctx.newJsonParser().parseResource(Task.class, body);
-    }
-
-    @Converter
-    public static ServiceRequest toServiceRequest(InputStreamCache inputStreamCache, Exchange exchange)
-            throws Exception {
-        FhirContext ctx = FhirContext.forR4();
-        String body = exchange.getContext().getTypeConverter().mandatoryConvertTo(String.class, inputStreamCache);
-        return (ServiceRequest) ctx.newJsonParser().parseResource(ServiceRequest.class, body);
-    }
-
-    @Converter
-    public static Observation toObservation(InputStreamCache inputStreamCache, Exchange exchange) throws Exception {
-        FhirContext ctx = FhirContext.forR4();
-        String body = exchange.getContext().getTypeConverter().mandatoryConvertTo(String.class, inputStreamCache);
-        return (Observation) ctx.newJsonParser().parseResource(Observation.class, body);
-    }
-
-    @Converter
-    public static DiagnosticReport toDiagnosticReport(InputStreamCache inputStreamCache, Exchange exchange)
-            throws Exception {
-        FhirContext ctx = FhirContext.forR4();
-        String body = exchange.getContext().getTypeConverter().mandatoryConvertTo(String.class, inputStreamCache);
-        return (DiagnosticReport) ctx.newJsonParser().parseResource(DiagnosticReport.class, body);
-    }
-
-    @Converter
     public static Bundle toBundle(InputStreamCache inputStreamCache, Exchange exchange) throws Exception {
         FhirContext ctx = FhirContext.forR4();
         String body = exchange.getContext().getTypeConverter().mandatoryConvertTo(String.class, inputStreamCache);
         return (Bundle) ctx.newJsonParser().parseResource(Bundle.class, body);
-    }
-
-    @Converter
-    public static Practitioner toPractitioner(InputStreamCache inputStreamCache, Exchange exchange) throws Exception {
-        FhirContext ctx = FhirContext.forR4();
-        String body = exchange.getContext().getTypeConverter().mandatoryConvertTo(String.class, inputStreamCache);
-        return (Practitioner) ctx.newJsonParser().parseResource(Practitioner.class, body);
     }
 }
